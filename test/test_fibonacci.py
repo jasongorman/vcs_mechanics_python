0677d974aa5748e5e60e184803e846b235de2a8d
Jason Gorman <jason.gorman@codemanship.com>
2024-02-01 12:16:22 +0000

    Added test for third fibonacci number

--- test/test_fibonacci.py
+++ test/test_fibonacci.py
@@ -7,8 +7,11 @@ class FibonacciTest(unittest.TestCase):
     def test_first_number_is_zero(self):
         self.assertEqual(0, fibonacci(0))
 
-    def test_first_number_is_zero(self):
+    def test_second_number_is_one(self):
         self.assertEqual(1, fibonacci(1))
+
+    def test_third_number_is_one(self):
+        self.assertEqual(1, fibonacci(2gi))
 
 
 if __name__ == '__main__':
