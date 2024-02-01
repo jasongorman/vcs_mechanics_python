d0ba4ca25345013ec30fc5b77271ac600c154ab9
Jason Gorman <jason.gorman@codemanship.com>
2024-02-01 12:14:51 +0000

    Added test for second fibonacci number

--- test/test_fibonacci.py
+++ test/test_fibonacci.py
@@ -7,6 +7,9 @@ class FibonacciTest(unittest.TestCase):
     def test_first_number_is_zero(self):
         self.assertEqual(0, fibonacci(0))
 
+    def test_first_number_is_zero(self):
+        self.assertEqual(1, fibonacci(1))
+
 
 if __name__ == '__main__':
     unittest.main()
