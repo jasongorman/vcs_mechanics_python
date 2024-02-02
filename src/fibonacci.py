e25c422e0f80ff9564ff06c5c693a98a1bedff24
Jason Gorman <jason.gorman@codemanship.com>
2024-02-02 09:03:04 +0000

    Replaced recursive fibonacci algorithm with faster iterative solution

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,4 +1,15 @@
 def fibonacci(n):
     if n < 2:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+
+    fib_minus_one = 1
+    fib_minus_two = 0
+    fib = 0
+
+    for index in range(2, n + 1):
+        fib = fib_minus_one + fib_minus_two
+        fib_minus_two = fib_minus_one
+        fib_minus_one = fib
+
+    return fib
+
